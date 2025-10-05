26f1523f679a69b53efb7e83b293840f384b3f98
Domenic Dvojmoc <applecakes811@gmail.com>
2025-10-05 14:22:16 +0200

    Commiting beginning file struct

---
+++ main.c
@@ -0,0 +1,5 @@
+//
+// Created by domenic on 10/5/25.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by domenic on 10/5/25.
+//
+
+#ifndef SSHPOTICA_MAIN_H
+#define SSHPOTICA_MAIN_H
+
+#endif //SSHPOTICA_MAIN_H
